96f260fec3080bc7951b333df61beb64244dd0e3
uller82 <christian.ulrich82@gmail.com>
2018-04-30 13:03:33 +0200

    easy exports

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,4 +1,4 @@
-export { BaseStats, DefenseStats, Gender, IDefenseStats, IHitLocation, ISurvivor, IBaseStats, ISurvivorBaseStat } from "./survivor";
-export { ISettlement } from "./settlement";
-export { IGearGrid, IGridSlot } from "./gear";
-export { ID } from "./generics";
+export * from "./survivor";
+export * from "./settlement";
+export * from "./gear";
+export * from "./generics";
