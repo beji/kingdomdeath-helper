e2210d09743552f206dbf2226dd3ae18ed6539d8
Björn Erlwein <bjoernerlwein@gmail.com>
2018-05-13 13:54:40 +0200

    No need for the home Link here anymore

--- src/pages/suvivorcard.tsx
+++ src/pages/suvivorcard.tsx
@@ -1,10 +1,7 @@
-import { ID, ISettlement } from "interfaces";
-import React from "react";
-import { Fragment } from "react";
+import { ID } from "interfaces";
+import React, { Fragment } from "react";
 import { RouteComponentProps } from "react-router";
 import GearGrid from "../components/GearGrid";
-import Link from "../components/Link";
-import { FancyButton } from "../components/StyledComponents";
 
 interface IPageMatchParams {
     cardnumber: string;
@@ -17,7 +14,6 @@ class SurvivorCardPage extends React.Component<ISurvivorCardPageProps> {
         const { match } = this.props;
         return (
             <Fragment>
-                <Link to="/"><FancyButton>Home</FancyButton></Link>
                 <GearGrid id={match.params.cardnumber as ID} />
             </Fragment>);
     }
