861dd114ac1c7982f6d73ee952e6f4f59f6e7388
Björn Erlwein <bjoernerlwein@gmail.com>
2018-05-13 13:55:58 +0200

    Fix the import

--- src/pages/suvivorcard.tsx
+++ src/pages/suvivorcard.tsx
@@ -1,5 +1,5 @@
 import { ID } from "interfaces";
-import React, { Fragment } from "react";
+import React from "react";
 import { RouteComponentProps } from "react-router";
 import GearGrid from "../components/GearGrid";
 
@@ -13,9 +13,9 @@ class SurvivorCardPage extends React.Component<ISurvivorCardPageProps> {
     public render() {
         const { match } = this.props;
         return (
-            <Fragment>
+            <React.Fragment>
                 <GearGrid id={match.params.cardnumber as ID} />
-            </Fragment>);
+            </React.Fragment>);
     }
 }
 
